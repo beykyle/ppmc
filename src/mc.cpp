d58261e96b73d610289eaed70637cab0f0cd4c1d
beykyle <beykyle@umich.edu>
2021-04-19 18:46:38 -0400

    Corrected area calculation

---
+++ src/mc.cpp
@@ -0,0 +1,32 @@
+#include "mc.hpp"
+
+using namespace mc;
+
+
+double mc::mean(double r, uint64_t count) {
+  return r/static_cast<double>(count);
+};
+
+double mc::stddev(double r, double r2, uint64_t count){
+  const double ccount = static_cast<double>(count);
+  return std::sqrt((r2 - r * r / ccount ) / (ccount*ccount));
+};
+
+double mc::relError(double r, double r2, uint64_t count) {
+  const double ccount = static_cast<double>(count);
+  return fabs(stddev(r,r2,count) * ccount / r);
+};
+
+double mc::vol(double length_tally, double height_tally, double width, uint64_t count) {
+  return mean(length_tally,count) * mean(height_tally,count) * width;
+};
+
+double mc::stddevVol(double length, double height, 
+                     double length2, double height2, 
+                     double width, uint64_t count) {
+  const double l = mean(length,count);
+  const double h = mean(height,count);
+  const double dl = stddev(length,length2,count);
+  const double dh = stddev(height,length2,count);
+  return width * sqrt(l*l*dh*dh + h*h*dl*dl);
+};
--- src/mc.hpp
+++ src/mc.hpp
@@ -4,55 +4,88 @@
 #include <limits>
 #include <cmath>
 #include <random>
+#include <cassert>
 
 namespace mc {
+//helpers
+double mean(double r, uint64_t count);
+double stddev(double r, double r2, uint64_t count);
+double relError(double r, double r2, uint64_t count);
+double vol(double length_tally, double height_tally, 
+           double width, uint64_t count);
+double stddevVol(double length, double height, 
+                double length2, double height2, 
+                double width, uint64_t count);
 
-  using Uniform = std::uniform_real_distribution<>;
+using Uniform = std::uniform_real_distribution<>;
 
-  template<class Func, class YBoundLow, class YBoundUp>
-  std::pair<double,double> 
-  Integrate2D(Func f, 
-              YBoundLow lower, YBoundUp upper, 
-              double x_lower, double x_upper,
-              double tol) {
-    
-    // initialize RNG
-    std::random_device rd;  
-    std::mt19937 gen(rd()); 
-    Uniform dx(x_lower, x_upper);
+template<class Func, class YBoundLow, class YBoundUp>
+std::pair<double,double> 
+Integrate2D(Func f, 
+            YBoundLow lower, YBoundUp upper, 
+            double x_lower, double x_upper,
+            double tol, 
+            bool loud = false, uint32_t print_freq = 10000) {
+  
+  // initialize RNG
+  std::mt19937 gen(389648901368); 
+  Uniform dx(x_lower, x_upper);
+  
+  // initialize length tallies
+  assert(x_upper > x_lower);
+  const double width = x_upper - x_lower;
+  double length  = 0;
+  double length2 = 0;
 
-    // initialize tallies
-    uint64_t count  = 0;
-    double   res    = 0;
-    double   res_2  = 0;
-    double   old_res = std::numeric_limits<double>::max();
+  // initialize height tallies
+  double height  = 0;
+  double height2 = 0;
+  
+  // initialize volume tallies
+  double curr_vol   = 0;
+  double stddev_vol = 0;
+  double old_vol    = std::numeric_limits<double>::max();
+  double dvol       = old_vol;
+  // initialize counter
+  uint64_t count = 0;
 
-    auto stddev = [](double r, double r2, uint64_t count){
-      const double ccount = static_cast<double>(count);
-      return (r2 - r * r / ccount ) / (ccount*ccount);
-    };
-    
-    // loop unitl convergence
-    do{
-      // sample independent vars
-      const auto x = dx(gen);
-      const auto y = Uniform{lower(x),upper(x)}(gen);
+  // loop until convergence
+  do{
+    // sample independent vars
+    const auto x = dx(gen);
+    const auto y = Uniform{lower(x),upper(x)}(gen);
 
-      // score tallies
-      const auto val = f(x,y);
-      old_res = res;
-      res    += val;
-      res_2  +=  val*val;
-      ++count;
+    // score length tallies
+    const double smpl_length = upper(x) - lower(x);
+    length   += smpl_length;
+    length2  += smpl_length*smpl_length;
 
-      // print progress
-      if (count % 100 == 0) 
-        std::cout << "N  : " << count 
-                  << "\ntol: " << tol << "\n" << std::flush; 
+    // score height tallies
+    const auto smpl_height = f(x,y);
+    height   += smpl_height;
+    height2  += smpl_height*smpl_height;
+
+    // incrememnt counter
+    ++count;
+
+    // assess convergence
+    curr_vol = vol(length,height,width,count);
+    dvol = fabs((curr_vol - old_vol)/old_vol);
+    old_vol = curr_vol;
+
+    // print current tallies
+    if (loud and count % print_freq == 0) {
+      stddev_vol = stddevVol(length,height,length2,height2,width,count);
+      std::cout << count 
+                << "\nval     : " << curr_vol
+                << " +/- "        << stddev_vol
+                << "\ntol     : " << dvol << "\n\n" << std::flush; 
     }
-    while ( stddev(res, res_2, count) > tol );
-    
-    return {res, stddev(res, res_2, count)};
   }
+  while ( dvol > tol or count < 10);
+  
+  std::cout << "Converged in: " << count << " iterations. \n";
+  return {curr_vol, stddevVol(length,height,length2,height2,width,count)};
+}
 
 }
