0d450bd022c17e58ecc7ac6b4eebe95a8acbd9f1
beykyle <beykyle@umich.edu>
2021-04-19 20:18:45 -0400

    Added low mass cutoffs

--- src/ppmc.cpp
+++ src/ppmc.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <vector>
 
 #include "elhq.hpp"
 #include "mc.hpp"
@@ -16,6 +17,11 @@ int main() {
                             (9 * s); // [fb/TeV]
   
   std::cout << "Calculating pp->mu+mu-...\n" << std::flush;
+
+  // this integral was calculated in the high energy limit (m_mu ~ mu_q -> 0),
+  // so it diverges at low energy. 
+  // We need to use low-energy cutoffs for parton COM frame energy^2 (shat)
+  std::vector<double> Mcut {0.01, 0.1, 1.0}; // Tev
   
   // set up ELHQ parton distribution functions
   const auto u    = elhq::Uv{} + elhq::Us{};
@@ -23,34 +29,38 @@ int main() {
   const auto dbar = elhq::Dbars{};
   const auto ubar = elhq::Ubars{};
 
-  // set up integrator
-  auto [result, stddev] = mc::Integrate2D(
-       [u,d,dbar,ubar,s](double shat, double x) -> double
-       {
-         const double sfrac = shat/(x*s);
-         return 1/(x*shat) * 
-                (   (4.0/9.0) * ( u(x) * ubar(sfrac) + ubar(x) * u(sfrac)) 
-                  + (1.0/9.0) * ( d(x) * dbar(sfrac) + dbar(x) * d(sfrac))
-                );
-       },
-       [s](double shat) { return shat/s; },
-       [ ](double shat) { return 1.;     },
-       1,s,
-       1E-14,
-       false
-      );
+  for (const auto cutoff : Mcut) {
+    // set up integrator
+    auto [result, stddev] = mc::Integrate2D(
+         [u,d,dbar,ubar,s](double shat, double x) -> double
+         {
+           const double sfrac = shat/(x*s);
+           return 1/(x*shat) * 
+                  (   (4.0/9.0) * ( u(x) * ubar(sfrac) + ubar(x) * u(sfrac)) 
+                    + (1.0/9.0) * ( d(x) * dbar(sfrac) + dbar(x) * d(sfrac))
+                  );
+         },
+         [s](double shat) { return shat/s; },
+         [ ](double shat) { return 1.;     },
+         cutoff*cutoff,s,
+         1E-14,
+         false
+        );
 
-  result *=  factor;
-  stddev *=  factor;
+    result *=  factor;
+    stddev *=  factor;
 
-  std::cout << "sigma [fb]: " 
-            << std::scientific
-            << std::setprecision(7)
-            << result << " +/- " 
-            << std::scientific
-            << std::setprecision(7)
-            << stddev << " +/- " << "\n";
+    std::cout << "Low energy cutoff [TeV]: " << cutoff
+              << "\nsigma [fb]: " 
+              << std::scientific
+              << std::setprecision(7)
+              << result << " +/- " 
+              << std::scientific
+              << std::setprecision(7)
+              << stddev << " +/- " << "\n";
   
+  }
+
   // convergence test
   // TODO
 }
